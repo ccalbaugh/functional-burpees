8cff70eee7139a542aef182a0b1fe53cba244cae
ccalbaugh <calbaugh@qca-sdc.com>
2018-01-23 11:35:53 -0600

    add try/catch blocks for async funcs

--- challenge-area/challenge.js
+++ challenge-area/challenge.js
@@ -16,55 +16,108 @@ async function fetchData(url) {
 const fetchedData = fetchData(BASE_URL);
 
 async function replaceQuotes(data) {
-    const list = await data;
-    const replacedQuotes = list.map( result => result.question = result.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'") );
-    console.log(`replacedQuotes: `, replacedQuotes);
+    let list;
+    try {
+        list = await data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        const replacedQuotes = list.map( result => result.question = result.question.replace(/&quot;/g, '"').replace(/&#039;/g, "'") );
+        console.log(`replacedQuotes: `, replacedQuotes);
+        return replacedQuotes;
+    }
 }
 
 replaceQuotes(fetchedData);
 
-async function filterByAttr(data, attr, filterVal) {
-    const list = await data;
-    const filteredList = list.filter( item => item[attr] === filterVal );
-    console.log(`filteredList: `, filteredList);
+async function pluckByAttr(data, attr, filterVal) {
+    let list;
+    try {
+        list = await data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        const filteredList = list.filter( item => item[attr] === filterVal );
+        console.log(`filteredList: `, filteredList);
+        return filteredList;
+    }
 }
 
-const filterByDifficulty = filterByAttr(fetchedData, 'difficulty', 'easy');
-filterByDifficulty;
+const pluckByDifficulty = pluckByAttr(fetchedData, 'difficulty', 'easy');
+pluckByDifficulty;
 
 async function sortByDifficulty(data) {
-    const list = await data;
-    const sortedByDifficulty = [...list].sort((a, b) => {
-        if (a.difficulty === 'easy' || b.difficulty === 'hard') { return -1; } 
-        else if (a.difficulty === 'hard' || b.difficulty === 'easy') { return 1; }  
-        else { return 0; }
-    });
-    console.log(`list: `, list);
-    console.log(`sortedByDifficulty: `, sortedByDifficulty);
+    let list;
+    try {
+        list = await data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        const sortedByDifficulty = [...list].sort((a, b) => {
+            if (a.difficulty === 'easy' || b.difficulty === 'hard') { return -1; } 
+            else if (a.difficulty === 'hard' || b.difficulty === 'easy') { return 1; }  
+            else { return 0; }
+        });
+        console.log(`list: `, list);
+        console.log(`sortedByDifficulty: `, sortedByDifficulty);
+        return sortedByDifficulty;
+    }
 }
 
 sortByDifficulty(fetchedData);
 
-async function numOfEachDiff(data) {
-    const list = await data;
-    const nums = list.reduce((sum, curr) => {
-        const difficulty = curr.difficulty;
-        return Object.assign(sum, { [difficulty]: sum[difficulty] + 1 });
-    }, { 
-        "easy": 0, 
-        "medium": 0, 
-        "hard": 0
-    });
-    console.log(`nums: `, nums);
+async function countNumOfAttrs(data, attr) {
+    let list;
+    try {
+        list = await data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        const nums = list.reduce((sum, curr) => {
+            const match = curr[attr]
+            return Object.assign(sum, { [match]: (sum[match] + 1) });
+        }, { 
+            "easy": 0, 
+            "medium": 0, 
+            "hard": 0
+        });
+        console.log(`nums: `, nums);
+        return nums;
+    }
 }
 
-numOfEachDiff(fetchedData);
+countNumOfAttrs(fetchedData, 'difficulty');
 
 async function checkSameCategory(data, category) {
-    const list = await data;
-    const checked = list.every( item => item.category === category );
-    console.log(`checked: `, checked);
+    let list;
+    try {
+        list = await data;    
+    } catch (error) {
+        console.error(error);
+    } finally {
+        const checked = list.every( item => item.category === category );
+        console.log(`checked: `, checked);
+        return checked;
+    }
 }
 
 const checkForCompSci = checkSameCategory(fetchedData, "Science: Computers");
 checkForCompSci;
+
+async function grabAndSort(data, grabBy, filterVal) {
+    let list;
+    try {
+        list = await data;
+    } catch (error) {
+        console.error(error);
+    } finally {
+        const grabbed = await pluckByAttr(list, grabBy, filterVal);
+        const sorted = grabbed.sort((a, b) => {
+            return a > b;
+        });
+        console.log(`sorted: `, sorted);
+        return sorted;
+    }
+}
+
+grabAndSort(fetchedData, 'difficulty', 'medium');
